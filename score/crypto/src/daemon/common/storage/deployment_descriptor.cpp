cee71da7add7c06180f834904fe53d1cb4127691
Athul Mallappallil <athulmssai@gmail.com>
2026-07-23 13:04:44 +0200

    Make shared deployment loader and writer

---
+++ score/crypto/src/daemon/common/storage/deployment_descriptor.cpp
@@ -0,0 +1,19 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+#include "score/crypto/src/daemon/common/storage/deployment_descriptor.hpp"
+
+namespace score::crypto::daemon::common::storage
+{
+const std::string DeploymentDescriptor::kEmptyString{};
+}  // namespace score::crypto::daemon::common::storage
